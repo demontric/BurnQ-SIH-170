5581d7779dfb4c76923de0c6e0b21788e90e8c9f
Aritra Roy <contact.aritra2006@gmail.com>
2026-09-26 00:53:46 +0530

    fixed table format error

--- frontend/src/components/ui/table.jsx
+++ frontend/src/components/ui/table.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "cn"
+import { cn } from "@/lib/utils"
 
 function Table({
   className,
@@ -8,7 +8,7 @@ function Table({
   return (
     <div
       data-slot="table-container"
-      className="relative w-full"
+      className="relative w-full overflow-x-auto"
     >
       <table
         data-slot="table"
@@ -69,7 +69,7 @@ function TableRow({
     <tr
       data-slot="table-row"
       className={cn(
-        "border-b transition-colors hover:bg-muted/50 has-aria-expanded:bg-muted/50 data-[state=selected]:bg-muted",
+        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
         className
       )}
       {...props}
@@ -85,7 +85,7 @@ function TableHead({
     <th
       data-slot="table-head"
       className={cn(
-        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0",
+        "h-10 px-2 text-left align-middle font-medium text-foreground whitespace-nowrap [&:has([role=checkbox])]:pr-0",
         className
       )}
       {...props}
@@ -101,7 +101,7 @@ function TableCell({
     <td
       data-slot="table-cell"
       className={cn(
-        "p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0",
+        "p-2 align-middle [&:has([role=checkbox])]:pr-0",
         className
       )}
       {...props}
